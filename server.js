c557bea61f07423879cbd3b77132d5d89b66dad1
Fatih <fatihce57@gmail.com>
2021-10-16 22:35:34 +0200

    cors intalled and added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors=require('cors')
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -12,6 +13,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors())
 
 const booksRouter = require('./routes/book')
 app.use('/books', booksRouter)
